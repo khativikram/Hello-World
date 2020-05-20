1117f42074704d93eb6adb0825b20c2246d2ed74
742180 <742180@LT080979.cts.com>
2020-05-20 11:37:50 +0530

    Initial commit

---
+++ src/main/java/com/gcp/helloWorldSample/HelloWorldSampleApplication.java
@@ -0,0 +1,13 @@
+package com.gcp.helloWorldSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcp/helloWorldSample/HelloWorldSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcp.helloWorldSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
